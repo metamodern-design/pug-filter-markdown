c640f11df020e3771c51a1a7a903f70065390924
Daniel Narey <danielnarey@users.noreply.github.com>
2020-06-01 16:56:02 -0400

    sync

--- test/index.js
+++ test/index.js
@@ -3,6 +3,11 @@ import pug from 'pug';
 import md from '../lib/index.js';
 
 
+const render = (str) => pug.render(str, {
+  filters: { md },
+}).trim();
+
+
 const pugString = `
 div.text-center
   :md(inline tag='h1' class='text-lg text-red')
@@ -13,8 +18,25 @@ div.text-center
 const expected = `<div class="text-center"><h1 class="text-lg text-red">This is <em>Markdown</em></h1></div>`;
 
 
-test('Renders Markdown inside Pug', async (t) => {
-  const result = pug.render(pugString, { filters: { md }}).trim();
+test('Renders single paragraph', async (t) => {
+  t.is(
+    render('div\n  :md\n    This is *Markdown*'),
+    '<div><p>This is <em>Markdown</em></p></div>',
+  );
+});
 
-  t.is(result, expected);
+
+test('Renders multiple paragraphs', async (t) => {
+  t.is(
+    render('div\n  :md\n    Hello, World!\n    This is *Markdown*'),
+    '<div><p>Hello, World!</p><p>This is <em>Markdown</em></p></div>',
+  );
+});
+
+
+test('Renders inline Markdown', async (t) => {
+  t.is(
+    render('h1\n  :md\n    This is *Markdown*'),
+    '<h1>This is <em>Markdown</em></h1>',
+  );
 });
